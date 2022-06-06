89cdffbedbd8df882cc44c76be7b35d94833195c
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-06 21:32:26 +0500

    header and footer always same on every page this is not page

--- src/Components/Routing/Routing.js
+++ src/Components/Routing/Routing.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter as Router, Route , Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // pages for navigations
 import Header from "../Components/Header";
@@ -10,13 +10,21 @@ import Company from "../Components/Company";
 export default function Routing() {
   return (
     <div>
-        <Router>
-            <Switch>
-                <Route path='/'>
-
-                </Route>
-            </Switch>
-        </Router>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/Company">
+            <Company />
+          </Route>
+          <Route path="/About">
+            <About />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
-  )
+  );
 }
