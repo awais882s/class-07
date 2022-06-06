78fa25ede1fab8eb12e603a3e059c3d361735a1b
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-06 22:41:11 +0500

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <div>
-      <Link to="">Company Logo</Link>
-      <Link to="">Home</Link>
-      <Link to="">Company</Link>
-      <Link to="">About</Link>
+      <Link to="/">Company Logo</Link>
+      <Link to="/">Home</Link>
+      <Link to="/Company">Company</Link>
+      <Link to="/About">About</Link>
     </div>
   );
 }
