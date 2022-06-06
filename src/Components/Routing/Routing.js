86d247ded6a9611423f27634b270902461516162
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-06 19:57:36 +0500

    Routings

---
+++ src/Components/Routing/Routing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Route , Switch } from 'react-router-dom';
+
+// pages for navigations
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import Home from "../Components/Home";
+import About from "../Components/About";
+import Company from "../Components/Company";
+export default function Routing() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
