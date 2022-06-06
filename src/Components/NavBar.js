aca03cbfe42188ff476b4447a7c603265bd873c7
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-06 22:52:37 +0500

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -8,6 +8,11 @@ export default function NavBar() {
       <Link to="/">Home</Link>
       <Link to="/Company">Company</Link>
       <Link to="/About">About</Link>
+      <Link to="/About">Contact</Link>
+      <Link to="/About">Register</Link>
+      <Link to="/About">Login</Link>
+
+
     </div>
   );
 }
