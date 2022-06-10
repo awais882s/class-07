1adf140108dc6eb11694ad8fe9b226bf87024a83
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-10 10:38:10 +0500

    update

--- src/Pages/Sections.js
+++ src/Pages/Sections.js
@@ -7,8 +7,8 @@ export default function Sections() {
             <h4 className='text-dark'>How to Make Automation In Firebase</h4>
             <p className='text-dark'>1.1st you go firebase hosting folder where you deploy already website
 
-                2.Copy file firebase.json and .firebaserc into main folder <br/>
-                3.Open file firebase.json where build you type (&& firebase deploy)<br/>
+                2.Copy file firebase.json and .firebaserc into main folder <br />
+                3.Open file firebase.json where build you type (&& firebase deploy)<br />
                 4.Again npm run build command
             </p>
 
