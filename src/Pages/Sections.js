da116f5a82c7d05b3c55ccb6339c23a080201913
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-10 10:03:36 +0500

    automations

--- src/Pages/Sections.js
+++ src/Pages/Sections.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
 export default function Sections() {
-  return (
-    <div>
-        Sections
-        <h1 className='text-dark'>How to Make Automation In Firebase</h1>
-        <p>1st you go firebase hosting folder where you deploy already website
+    return (
+        <div>
+            Sections
+            <h4 className='text-dark'>How to Make Automation In Firebase</h4>
+            <p className='text-dark'>1.1st you go firebase hosting folder where you deploy already website
 
+                2.copy file firebase.json and .firebaserc into main folder <br/>
+                3.open file firebase.json where build you type (&& firebase deploy)
+            </p>
 
-        </p>
-
-    </div>
-  )
+        </div>
+    )
 }
