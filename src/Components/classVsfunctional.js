5a1e757d239a1b7ebdd36acd9338f19c9a61c1d0
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-10 20:16:19 +0500

    Update classVsfunctional.js

--- src/Components/classVsfunctional.js
+++ src/Components/classVsfunctional.js
@@ -1,20 +1,38 @@
 // class components
-import React, { Component } from 'react'
-import { useState } from 'react';
+import React, { Component, useState } from 'react'
 
 export default class Students extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            stuname: "AWAIS S",
+            students: ["AWAIS S", "PRINCE", "SHAHZADA"]
+        }
+    }
+    onChangeHandler = (e) => {
+        this.setState({ stuName: e.target.value })
+    }
     render() {
         return (
             <div>
                 List of Students
+                <p>Student Name:{this.state.stuname}</p>
+                <input type="text" placeholder='Student Name:' onChange={this.onChangeHandler} />
+                {
+                    this.state.students.map((customerName) => {
+                        return (
+                            <p>{customerName}</p>
+                        )
+                    })
+                }
             </div>
         )
     }
 }
-
+// functional components
 export function StudentsFunction() {
-    const [name, setName] = useState("AWAIS S")
-    const customers = ["awais s", "waqar", "hamza"];
+    const [stuname, setName] = useState("AWAIS S")
+    const [students, setStudents] = useState(["awais s", "waqar", "hamza"]);
     const onChangeHandler = (e) => {
         setName(e.target.value);
     }
@@ -22,10 +40,10 @@ export function StudentsFunction() {
     return (
         <div>
             List of Students
-            <p>Name:{name}</p>
+            <p>Student Name:{stuname}</p>
             <input type="text" placeholder='Student Name:' onChange={onChangeHandler} />
             {
-                customers.map((customerName) => {
+                students.map((customerName) => {
                     return (
                         <p>{customerName}</p>
                     )
