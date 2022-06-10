4d7190c7c85fe4c1007be358233859643f06030f
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-10 11:36:39 +0500

    react toast

--- src/Pages/Sections.js
+++ src/Pages/Sections.js
@@ -1,17 +1,34 @@
 import React from 'react'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function Sections() {
+    const ctaHandler = () => {
+        toast.success('🦄 Wow so easy!', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+
+    }
     return (
         <div>
             Sections
             <h4 className='text-dark'>How to Make Automation In Firebase</h4>
-            <p className='text-dark'>1.1st you go firebase hosting folder where you deploy already website
+            <p className='text-dark'>1.1st you go firebase hosting folder where you deploy already website <br />
 
                 2.Copy file firebase.json and .firebaserc into main folder <br />
                 3.Open file firebase.json where build you type (&& firebase deploy)<br />
                 4.Again npm run build command
             </p>
-
+            <button type="button" class="btn btn-warning" onClick={ctaHandler}>
+                Call to Actions
+            </button>
         </div>
     )
 }
